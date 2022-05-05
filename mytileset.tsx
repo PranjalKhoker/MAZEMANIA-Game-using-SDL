cf2ba19924414ab4f9b5c54568d317912130e66d
pranjal <pranjalkhoker12345@gmail.com>
2022-05-05 18:45:48 +0530

    sixth commit

--- mytileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="mytileset" tilewidth="32" tileheight="32" tilecount="16384" columns="128">
- <image source="../mytileset.png" width="4096" height="4096"/>
-</tileset>
